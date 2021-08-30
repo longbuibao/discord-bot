81f7ddadc44e1da52044ce2ebe9db3e6798c2631
longbuibao <blong1102@gmail.com>
2021-08-31 01:30:41 +0700

    dummy commit

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents } = require('discord.js')
 const { token } = process.env
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
 client.once('ready', () => {
-    console.log('Ready!');
+    console.log('Ready!')
 });
 
 client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
+    if (!interaction.isCommand()) return
 
-    const { commandName } = interaction;
+    const { commandName } = interaction
 
     if (commandName === 'ping') {
-        await interaction.reply('Pong!');
+        await interaction.reply('Pong!')
     } else if (commandName === 'server') {
-        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`)
     } else if (commandName === 'user') {
-        await interaction.reply('User info.');
+        await interaction.reply('User info.')
     }
-});
+})
 
-client.login(token);
+client.login(token)
