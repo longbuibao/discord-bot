1ecee367084b3feb0c5134e5089abe2bffdb2252
longbuibao <blong1102@gmail.com>
2021-08-31 12:30:52 +0700

    commands folder structure

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const { Client, Intents } = require('discord.js')
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = process.env
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
 
 client.once('ready', () => {
     console.log('Ready!')
@@ -9,16 +19,17 @@ client.once('ready', () => {
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return
+    const command = client.commands.get(interaction.commandName);
 
-    const { commandName } = interaction
+    if (!command) return;
 
-    if (commandName === 'ping') {
-        await interaction.reply('Pong!')
-    } else if (commandName === 'server') {
-        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`)
-    } else if (commandName === 'user') {
-        await interaction.reply('User info.')
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
+
 })
 
 client.login(token)
