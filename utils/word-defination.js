51c07e9fffbe23dec74b73512e86fd9f7b19c0fd
longbuibao <blong1102@gmail.com>
2021-09-13 14:03:31 +0700

    lazy :')

--- utils/word-defination.js
+++ utils/word-defination.js
@@ -7,7 +7,7 @@ module.exports = (data) => {
     const defs = meanings.reduce((acc, curr) => {
         const { partOfSpeech, definitions } = curr
         const def = definitions.map(def => {
-            return `\n\n**Defination: **${def.definition || ":pleading_face: We have no defination for this defination :pleading_face:"}, **Example: **${def.example || ":pleading_face: We have no example for this word :pleading_face:"}`
+            return `\n\n**Defination: **${def.definition || ":pleading_face: We have no defination for this defination :pleading_face:"}, **Example: **${def.example || ":pleading_face: We have no example for this defination :pleading_face:"}`
         }).toString().replace(',', '')
         acc.push({ name: `\n\n**:pushpin:${partOfSpeech}:pushpin:**\n\n`, value: def.length > 1024 ? def.substring(0, 1023) : def })
         return acc
