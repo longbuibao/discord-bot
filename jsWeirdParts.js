62a6e7b80e200fdcd91f8a11b7b4e7269a900800
longbuibao <blong1102@gmail.com>
2021-10-01 18:57:26 +0700

    update

--- jsWeirdParts.js
+++ jsWeirdParts.js
@@ -1,9 +1,1 @@
-document.getElementById('sendButton').addEventListener('click', () => alert('hello'))
-
-let n = 0
-
-//this need to be done, then the click event can be execute
-while (n < 10e+4) {
-    n++
-    console.log(n)
-}
+console.log(1 && console.log('1'))
