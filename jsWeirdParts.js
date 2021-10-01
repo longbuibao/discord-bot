eb058857250966395f3e26ef9537a8930c3df09f
longbuibao <blong1102@gmail.com>
2021-10-01 12:17:57 +0700

    js lexical environment

--- jsWeirdParts.js
+++ jsWeirdParts.js
@@ -1,2 +1,20 @@
-let a;
-if (a === undefined) { console.log('a is undefined') }
+function a() {
+    b()
+    var myVar = 2
+
+    function b() {
+        console.log(this.myVar)
+        console.log(myVar)
+    }
+
+    var obj = {
+        method: function() {
+            console.log(this)
+        }
+    }
+
+    obj.method()
+}
+
+var myVar = 1
+a()
