f0d2ae93496bee7a43170b626f6041339524842d
longbuibao <blong1102@gmail.com>
2021-10-03 23:03:00 +0700

    learned something that I have learned :v

--- jsWeirdParts.js
+++ jsWeirdParts.js
@@ -1,14 +1,14 @@
-const a = {
-    name: 'long',
-    age: 12
-}
-console.log(JSON.parse(JSON.stringify(a)).name)
+// const a = {
+//     name: 'long',
+//     age: 12
+// }
+// console.log(JSON.parse(JSON.stringify(a)).name)
 
-const b = {
-    "name": "long"
-}
+// const b = {
+//     "name": "long"
+// }
 
-console.log(b.name)
+// console.log(b.name)
 
 //make a string like object in javasript istead of xml
 //string of data but look like object literals
@@ -21,6 +21,24 @@ console.log(b.name)
 //everything you can do with other types (object, string, number, boolean), you can do with function
 //e.g assign them with variable, pass them arround, create them
 
-const func = function hello1() { console.log('hello') }
-func()
-console.log(func.name)
+// const func = function hello1() { console.log('hello') }
+// func()
+// console.log(func.name)
+
+
+//this will point to different object, different thing, depending on how a function is called
+
+var c = {
+    name: 'c object',
+    log: function() {
+        this.name = 'updated c object'
+
+        var setname = function(who) {
+            who.name = 'updated again c object'
+        }
+
+        setname(this)
+    }
+}
+c.log()
+console.log(c)
