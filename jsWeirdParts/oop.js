d9d725109f5d9ad0a4aa5acd7220574f4b295d21
longbuibao <blong1102@gmail.com>
2021-10-07 16:34:36 +0700

    chapter 58

--- jsWeirdParts/oop.js
+++ jsWeirdParts/oop.js
@@ -1,41 +1,60 @@
-const person = {
-    name: 'defaul',
-    age: 15,
-    getNameAndAge: function() {
-        console.log(this)
-        return this.name + ' ' + this.age
-    }
+// const person = {
+//     name: 'defaul',
+//     age: 15,
+//     getNameAndAge: function() {
+//         console.log(this)
+//         return this.name + ' ' + this.age
+//     }
+// }
+
+// const john = {
+//     name: 'john',
+//     age: 31
+// }
+
+// // never do this __proto__, for demo purpose only
+
+// john.__proto__ = person
+
+// console.log(john.getNameAndAge())
+// console.log(john.name)
+
+
+// function greeting() {
+//     console.log(this.name)
+// }
+
+// greeting.call = function() {
+//     console.log('this call is overrided')
+// }
+
+// greeting.call(john);
+
+// const obj = {
+//     name: 'png',
+//     text: 'hello'
+// }
+
+// for (const i in obj) {
+//     console.log(i)
+//     console.log(obj.__proto__)
+// }
+
+
+function User() {
+    this.name = 'long'
+    this.age = 15
 }
 
-const john = {
-    name: 'john',
-    age: 31
+User.prototype.getName = function() {
+    return this.name
 }
 
-// never do this __proto__, for demo purpose only
+// console.log(User.__proto__)
 
-john.__proto__ = person
+const Long = new User()
 
-console.log(john.getNameAndAge())
-console.log(john.name)
+// for (const key in Long) console.log(key)
 
-
-function greeting() {
-    console.log(this.name)
-}
-
-greeting.call = function() {
-    console.log('this call is overrided')
-}
-
-greeting.call(john);
-
-const obj = {
-    name: 'png',
-    text: 'hello'
-}
-
-for (const i in obj) {
-    console.log(i)
-    console.log(obj.__proto__)
-}
+// console.log(Long)
+console.log(Long.__proto__)
