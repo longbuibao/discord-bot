6648ee4a90594b5c6b603f4d9db598c8994c50b2
longbuibao <blong1102@gmail.com>
2021-10-09 01:35:35 +0700

    blush

--- http_definitive_guide/part_one/index.js
+++ http_definitive_guide/part_one/index.js
@@ -1,4 +1,4 @@
-const height = 7
+const height = 9
 const result = []
 
 const mulNumber = (n) => {
@@ -12,8 +12,6 @@ const mulNumber = (n) => {
     }, 1)
 }
 
-// console.log(mulNumber(7))
-
 for (let index = 0; index <= height; index++) {
     result.push(mulNumber(index) * mulNumber(index))
 }
@@ -22,5 +20,3 @@ for (let index = 0; index < result.length; index++) {
     const element = result[index];
     console.log(' '.repeat(result.length - index) + element)
 }
-
-// console.log(result)
