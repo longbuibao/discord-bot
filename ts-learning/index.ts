912ac2137f944da56a75358559faf29da0cd00bf
longbuibao <blong1102@gmail.com>
2021-10-12 22:54:55 +0700

    getting started

---
+++ ts-learning/index.ts
@@ -0,0 +1,18 @@
+import axios from "axios"
+
+const url = "https://jsonplaceholder.typicode.com/todos/1"
+
+interface Todo {
+  id: number
+  title: string
+  completed: boolean
+}
+
+axios.get(url).then((res) => {
+  const todo = res.data as Todo
+  logTodo(todo.id, todo.title, todo.completed)
+})
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(id, title, completed)
+}
