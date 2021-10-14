58d963254cc4bd5371f74cbab718ab66ceec89c1
longbuibao <blong1102@gmail.com>
2021-10-14 23:08:50 +0700

    why my map is not show up?

--- my-courses/ts-learning/maps/src/User.ts
+++ my-courses/ts-learning/maps/src/User.ts
@@ -1,17 +1,17 @@
 import faker from 'faker'
 
-class User {
+export class User {
   name: string
   location: {
     lat: number
-    long: number
+    lng: number
   }
 
   constructor() {
     this.name = faker.name.firstName()
     this.location = {
       lat: parseFloat(faker.address.latitude()),
-      long: parseFloat(faker.address.longitude()),
+      lng: parseFloat(faker.address.longitude()),
     }
   }
 }
