e662d7c2492a09d0a3025a16bc546f27beb41d62
longbuibao <blong1102@gmail.com>
2021-10-25 13:00:36 +0700

    console reporter

--- my-courses/ts-learning/reuseable-code/src/index.ts
+++ my-courses/ts-learning/reuseable-code/src/index.ts
@@ -1,10 +1,13 @@
 import { MatchReader } from './MatchReader'
 import { CsvFileReader } from './CsvFileReader'
+import { Summary } from './Summary'
 import { WinAnalysis } from './analyzers/WinAnalysis'
+import { ConsoleReport } from './reportTargets/ConsoleReport'
 
 const csvFileReader = new CsvFileReader('football.csv')
 const matchReader = new MatchReader(csvFileReader)
-const analysis = new WinAnalysis('Man United')
+
+const summary = new Summary(new WinAnalysis('West Ham'), new ConsoleReport())
 
 matchReader.load()
-console.log(analysis.run(matchReader.matches))
+summary.buildAndPrintReport(matchReader.matches)
