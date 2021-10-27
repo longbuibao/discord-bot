43cc73819235f975272d249d939e993f8f1ae0dd
longbuibao <blong1102@gmail.com>
2021-10-27 21:49:24 +0700

    events triggered

--- my-courses/ts-learning/cualar/src/index.ts
+++ my-courses/ts-learning/cualar/src/index.ts
@@ -4,4 +4,5 @@ const longbui = new User({ name: 'long bui', age: 21 })
 longbui.on('click', () => console.log('hello'))
 longbui.on('press', () => console.log('hello 2'))
 longbui.on('click', () => console.log('vcl'))
-console.log(longbui.events)
+
+longbui.trigger('click')
