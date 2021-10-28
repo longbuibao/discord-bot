e289ca6cf687916d90401008be28e9bd2f731ecf
longbuibao <blong1102@gmail.com>
2021-10-28 21:32:02 +0700

    have homework

--- my-courses/ts-learning/cualar/src/index.ts
+++ my-courses/ts-learning/cualar/src/index.ts
@@ -1,8 +1,11 @@
 import axios from 'axios'
+import { User } from './models/User'
 
 const url: string = 'http://localhost:3000'
 
-axios.post(url + '/users', {
+const user = new User({
   name: 'bui bao long',
   age: 21,
 })
+
+axios.post(url + '/users', user)
