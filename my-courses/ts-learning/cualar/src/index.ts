9fbe21e6ea1d1d2afd959fe37d60793ba425716c
longbuibao <blong1102@gmail.com>
2021-11-02 20:27:37 +0700

    :( not so focusing

--- my-courses/ts-learning/cualar/src/index.ts
+++ my-courses/ts-learning/cualar/src/index.ts
@@ -1,4 +1,9 @@
 import { User } from './models/User'
 
-const user = new User({})
-user.on('click', () => console.log('hello'))
+const user = new User({ name: 'long bui', age: 20 })
+
+user.on('change', () => {
+  console.log('user has change something')
+})
+
+user.set({ name: 'long bui', age: 25 })
