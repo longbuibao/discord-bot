da3165ee8557c8e49b09e373b6d8abedc0b32e61
longbuibao <blong1102@gmail.com>
2021-11-08 13:30:18 +0700

    View.ts

--- my-courses/ts-learning/cualar/src/views/UserForm.ts
+++ my-courses/ts-learning/cualar/src/views/UserForm.ts
@@ -1,16 +1,7 @@
 import { User } from '../models/User'
+import { View } from './View'
 
-export class UserForm {
-  constructor(public parent: Element, public model: User) {
-    this.bindModel()
-  }
-
-  bindModel() {
-    this.model.on('change', () => {
-      this.render()
-    })
-  }
-
+export class UserForm extends View {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
@@ -27,6 +18,8 @@ export class UserForm {
 
     if (input) {
       const name = input.value
+
+      if (name.length === 0) return alert('Please input your name')
       this.model.set({ name })
     }
   }
@@ -42,27 +35,5 @@ export class UserForm {
         <button class="set-age" >Set Random Age</button>
 		  </div>
 	  `
-  }
-
-  bindEvents(fragment: DocumentFragment) {
-    const eventsMap = this.eventsMap()
-    for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(':')
-
-      fragment.querySelectorAll(selector).forEach((element) => {
-        element.addEventListener(eventName, eventsMap[eventKey])
-      })
-    }
-  }
-
-  render(): void {
-    this.parent.innerHTML = ''
-
-    const templateElement = document.createElement('template')
-    templateElement.innerHTML = this.template()
-
-    this.bindEvents(templateElement.content)
-
-    this.parent.append(templateElement.content)
   }
 }
