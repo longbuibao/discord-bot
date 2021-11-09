17f4992a1638a7f2876f88b9db3abb4fcf79b507
longbuibao <blong1102@gmail.com>
2021-11-09 16:51:36 +0700

    kind of finised this section

--- my-courses/ts-learning/cualar/src/index.ts
+++ my-courses/ts-learning/cualar/src/index.ts
@@ -1,11 +1,15 @@
-import { UserForm } from './views/UserForm'
+import { UserEdit } from './views/UserEdit'
 import { User } from './models/User'
 
 const root = document.getElementById('root')
+const user = User.buildUser({ name: 'long buiiiiii', age: 21 })
 
 if (root) {
-  const form = new UserForm(root, User.buildUser({ name: 'NAME', age: 20 }))
-  form.render()
+  const userEdit = new UserEdit(root, user)
+
+  userEdit.render()
+
+  console.log(userEdit)
 } else {
   throw new Error('No root element')
 }
