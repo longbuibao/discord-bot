cb40a8e4329ea9da59583d04eb0300c3d3b52b3c
longbuibao <blong1102@gmail.com>
2021-11-10 21:07:37 +0700

    basic TS

--- my-courses/ts-learning/ts-express-auth/src/index.ts
+++ my-courses/ts-learning/ts-express-auth/src/index.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response } from 'express'
 import { router } from './routes/loginRoutes'
+import cookieSession from 'cookie-session'
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(
+  cookieSession({
+    keys: [''],
+  })
+)
 
 app.use(router)
 
