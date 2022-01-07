669d8e92eb947434bd8812ad4d587fe5b477eabc
bui bao long <blong1102@gmail.com>
2022-01-07 18:06:40 +0700

    getter from PropertyDescriptor

--- my-courses/ts-learning/new-course/ts-complier/src/app.ts
+++ my-courses/ts-learning/new-course/ts-complier/src/app.ts
@@ -1,26 +1,35 @@
-function someDecorator<T extends { new (...arg: any[]): {} }>(target: T) {
-  return class extends target {
-    age: number = 12
-    constructor(...arg: any[]) {
-      super(...arg)
-    }
+function AutoBind(
+  target: any,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value as Function
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      // A function which serves
+      // as a getter for the property, or
+      // undefined if there is no getter.
+      // When the property is accessed, this function is called without arguments
+      // and with this set to the object through which the property is accessed
+      // (this may not be the object on which the property is defined due to inheritance).
+      // The return value will be used as the value of the property. Defaults to undefined.
+      return originalMethod.bind(this)
+    },
+  }
+  return adjDescriptor
+}
+
+class Print {
+  message = 'This is a message'
+
+  @AutoBind
+  showMessage() {
+    console.log(this.message)
   }
 }
 
-function someDecoratorMore(target: any) {
-  console.log(target)
-}
-
-@someDecorator
-class Person {
-  name: string = 'long bui bao'
-  print() {
-    console.log(this.name)
-  }
-  constructor() {
-    console.log('creating person object...')
-  }
-}
-
-const person = new Person()
-console.log(person)
+const p = new Print()
+const button = document.querySelector('button')
+button?.addEventListener('click', p.showMessage)
