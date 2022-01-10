44056f6c4d02897be841ef8768e7274434f1ef52
bui bao long <blong1102@gmail.com>
2022-01-10 11:15:47 +0700

    active + finish

--- my-courses/ts-learning/new-course/ts-complier/src/app.ts
+++ my-courses/ts-learning/new-course/ts-complier/src/app.ts
@@ -1,25 +1,83 @@
+interface Validatable {
+  value: string | number
+  required?: boolean
+  minLength?: number
+  maxLength?: number
+  min?: number
+  max?: number
+}
+
+function validate(validatable: Validatable): boolean {
+  let isValid = true
+
+  if (validatable.required) {
+    isValid = isValid && validatable.value.toString().length !== 0
+  }
+
+  if (validatable.minLength != null && typeof validatable.value === 'string') {
+    isValid = isValid && validatable.value.length >= validatable.minLength
+  }
+
+  if (validatable.maxLength != null && typeof validatable.value === 'string') {
+    isValid = isValid && validatable.value.length <= validatable.maxLength
+  }
+
+  if (validatable.min != null && typeof validatable.value === 'number') {
+    isValid = isValid && validatable.value >= validatable.min
+  }
+
+  if (validatable.max != null && typeof validatable.value === 'number') {
+    isValid = isValid && validatable.value <= validatable.max
+  }
+
+  return isValid
+}
+
 function AutoBind(
   target: any,
   methodName: string,
   descriptor: PropertyDescriptor
 ) {
   const originalMethod = descriptor.value as Function
-<<<<<<< Updated upstream
   const adjeDescriptor: PropertyDescriptor = {
-=======
-  const newDescriptor: PropertyDescriptor = {
->>>>>>> Stashed changes
     configurable: true,
     enumerable: false,
     get() {
       return originalMethod.bind(this)
     },
   }
-<<<<<<< Updated upstream
   return adjeDescriptor
-=======
-  return newDescriptor
->>>>>>> Stashed changes
+}
+
+class ProjectList {
+  templateElement: HTMLTemplateElement
+  hostElement: HTMLDivElement
+  element: HTMLElement
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById(
+      'project-list'
+    ) as HTMLTemplateElement
+    this.hostElement = document.getElementById('app') as HTMLDivElement
+
+    const importedNode = document.importNode(this.templateElement.content, true)
+    this.element = importedNode.firstElementChild as HTMLElement
+    this.element.id = `${this.type}-projects`
+
+    this.attach()
+    this.renderContent()
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`
+    this.element.querySelector('ul')!.id = listId
+    this.element.querySelector('h2')!.textContent =
+      this.type.toUpperCase() + `PROJECTS`
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element)
+  }
 }
 
 class ProjectInput {
@@ -56,8 +114,12 @@ class ProjectInput {
   @AutoBind
   private submitHandler(event: Event) {
     event.preventDefault()
-
-    console.log(this.titleInputElement.value)
+    const userInput = this.gatherUserInput()
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput
+      console.log(title, desc, people)
+    }
+    this.clearInputs()
   }
 
   private configure() {
@@ -67,6 +129,46 @@ class ProjectInput {
   private attach() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element)
   }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value
+    const enteredDescription = this.descriptionInputElement.value
+    const enteredPeople = this.peopleInputElement.value
+
+    const titleValidatabele: Validatable = {
+      value: enteredTitle,
+      required: true,
+    }
+
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    }
+
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+    }
+
+    if (
+      !validate(titleValidatabele) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert('Invalid input')
+      return
+    } else return [enteredTitle, enteredDescription, +enteredPeople]
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = ''
+    this.descriptionInputElement.value = ''
+    this.peopleInputElement.value = ''
+  }
 }
 
 const projectInput = new ProjectInput()
+const activeProjectList = new ProjectList('active')
+const finishedProjectList = new ProjectList('finished')
