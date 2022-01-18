ebde83e8ea4d53398ad48fe77025205878a06c63
bui bao long <blong1102@gmail.com>
2022-01-18 17:04:04 +0700

    google map

--- my-courses/ts-learning/new-course/ts-complier/src/app.ts
+++ my-courses/ts-learning/new-course/ts-complier/src/app.ts
@@ -1,7 +1,27 @@
-import { ProjectInput } from './components/project-input'
-import { ProjectList } from './components/project-list'
+import axios from 'axios'
 
-new ProjectInput()
-new ProjectList('active')
-new ProjectList('finished')
-console.log('Yo')
+const form = document.querySelector('form')
+// const addressInput = document.getElementById('address') as HTMLInputElement
+declare var ol: any
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault()
+  //   const enterdAddress = addressInput.value
+  const coordinates = { lat: 40.41, lng: -73.99 } // Can't fetch coordinates from Google API, use dummy ones
+
+  document.getElementById('map')!.innerHTML = '' // clear <p> from <div id="map">
+  new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM(),
+      }),
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
+      zoom: 16,
+    }),
+  })
+}
+
+form?.addEventListener('submit', searchAddressHandler)
